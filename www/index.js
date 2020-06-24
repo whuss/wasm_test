fc6781f1b1b45f8392cfe67b65d64df5bcb497fb
Wilfried Huss <husswilfried@gmail.com>
2020-06-24 16:09:35 +0200

    call rust code from react

--- www/index.js
+++ www/index.js
@@ -1,17 +1,3 @@
-// import { Universe } from "wasm-game-of-life";
-
-// const pre = document.getElementById("game-of-life-canvas");
-// const universe = Universe.new();
-
-// const renderLoop = () => {
-//     pre.textContent = universe.render();
-//     universe.tick();
-
-//     requestAnimationFrame(renderLoop);
-// };
-
-// requestAnimationFrame(renderLoop);
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './src/App'
--- www/src/App.js
+++ www/src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import { Universe } from "wasm-game-of-life";
+
+// const pre = document.getElementById("game-of-life-canvas");
+// const universe = Universe.new();
+
+// const renderLoop = () => {
+//     pre.textContent = universe.render();
+//     universe.tick();
+
+//     requestAnimationFrame(renderLoop);
+// };
+
+// requestAnimationFrame(renderLoop);
+
+const Life = () => {
+    const [universe, setUniverse] = useState(Universe.new());
+
+    return <pre>{universe.render()}</pre>;
+}
 
 const App = ({ props }) => (
   <div>
     Webpack HMR Example
+    <Life/>
   </div>
 )
 
